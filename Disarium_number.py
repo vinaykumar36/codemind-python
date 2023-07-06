b6267f780a72687438d8d4981545fb471f4ace79
vinaykumar36 <138445209+vinaykumar36@users.noreply.github.com>
2023-07-06 10:13:56 +0530

    n=int(input()) a=str(n) b=a[::-1] c=int(b) e=[] g=[] j=1 while c:     d=c%10     c=c//10     e.append(d) for i in e:     f=i**j     g.append(f)     j+=1 if sum(g)==n:     print("True") else:     print("False")

--- Disarium_number.py
+++ Disarium_number.py
@@ -2,15 +2,18 @@ n=int(input())
 a=str(n)
 b=a[::-1]
 c=int(b)
-i=1
-f=[]
+e=[]
+g=[]
+j=1
 while c:
     d=c%10
     c=c//10
-    e=d**i 
-    i+=1
-    f.append(e)
-if sum(f)==n:
+    e.append(d)
+for i in e:
+    f=i**j
+    g.append(f)
+    j+=1
+if sum(g)==n:
     print("True")
 else:
     print("False")
